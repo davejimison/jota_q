a199db5ac8b6a02a3894a33fcca2ccd8a7f5a2b0
David Jimison <dave@fevercreative.com>
2017-11-17 20:37:37 -0800

    added various tests

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,21 @@
-var assert = require('assert');
+var {assert, expect} = require('chai')
+var should = require('chai').should()
+
 describe('Array', function() {
     describe('#indexOf()', function() {
         it('should return -1 when the value is not present', function() {
             assert.equal(-1, [1,2,3].indexOf(4));
         });
+
+        it('should also work with expect', function(){
+            expect('hermosa').to.be.a('string');
+            expect('te amo').to.have.lengthOf(6);
+        })
+
+        it('should also be good with should', function(){
+            var foo = 'jota';
+            foo.should.equal('jota')
+        })
     });
 });
 
